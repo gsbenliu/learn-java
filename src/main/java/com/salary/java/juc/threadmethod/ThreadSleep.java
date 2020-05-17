fb3012dd13e41c5e885d3d803ee1b300b1829bbd
liugang <540184059@qq.com>
2020-05-17 19:56:05 +0800

    线程方法！

---
+++ src/main/java/com/salary/java/juc/threadmethod/ThreadSleep.java
@@ -0,0 +1,21 @@
+package com.salary.java.juc.threadmethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadSleep {
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("thread sleep time ");
+                    try {
+                        TimeUnit.SECONDS.sleep(5L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+}
---
+++ src/main/java/com/salary/java/juc/threadmethod/ThreadYied.java
@@ -0,0 +1,26 @@
+package com.salary.java.juc.threadmethod;
+
+public class ThreadYied {
+
+    public static void main(String[] args) {
+        Thread thread_1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("A" + i);
+                    if (i % 10 == 0) Thread.yield();
+                }
+            }
+        });
+
+        Thread thread_2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("B" + i);
+                    if (i % 10 == 0) Thread.yield();
+                }
+            }
+        });
+        thread_1.start();
+        thread_2.start();
+    }
+}
