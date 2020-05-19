8625c64e93ba2b15f006d52f0b761b1ac8c3fbd0
liugang <540184059@qq.com>
2020-05-19 11:24:55 +0800

    synchronized 01 进行同步代码块的测试，以及字节码的实现验证。

--- src/main/java/com/salary/java/juc/_004/lock/sychronized.md
+++ src/main/java/com/salary/java/juc/_004/lock/sychronized.md
@@ -1,4 +1,5 @@
 #锁
+      https://www.cnblogs.com/LemonFive/p/11246086.html
       https://ifeve.com/java-synchronized/  
       1.Java中的每一个对象都可以作为锁。  
       2.对于同步方法，锁是当前实例对象。  
@@ -6,4 +7,9 @@
       4.对于同步方法块，锁是Synchonized括号里配置的对象。  
       5.当一个线程试图访问同步代码块时，它首先必须得到锁，退出或抛出异常时必须释放锁。那么锁存在哪里呢？锁里面会存储什么信息呢？
 ##同步的原理
-######JVM规范规定JVM基于进入和退出Monitor对象来实现方法同步和代码块同步，但两者的实现细节不一样。代码块同步是使用monitorenter和monitorexit指令实现，而方法同步是使用另外一种方式实现的，细节在JVM规范里并没有详细说明，但是方法的同步同样可以使用这两个指令来实现。monitorenter指令是在编译后插入到同步代码块的开始位置，而monitorexit是插入到方法结束处和异常处， JVM要保证每个monitorenter必须有对应的monitorexit与之配对。任何对象都有一个 monitor 与之关联，当且一个monitor 被持有后，它将处于锁定状态。线程执行到 monitorenter 指令时，将会尝试获取对象所对应的 monitor 的所有权，即尝试获得对象的锁。
+      JVM规范规定JVM基于进入和退出Monitor对象来实现方法同步和代码块同步，但两者的实现细节不一样。
+      代码块同步是使用monitorenter和monitorexit指令实现，而方法同步是使用另外一种方式实现的，细节在JVM规范里并没有详细说明，
+      但是方法的同步同样可以使用这两个指令来实现。
+      monitorenter指令是在编译后插入到同步代码块的开始位置，而monitorexit是插入到方法结束处和异常处， 
+      JVM要保证每个monitorenter必须有对应的monitorexit与之配对。任何对象都有一个 monitor 与之关联，当且一个monitor 被持有后，
+      它将处于锁定状态。线程执行到 monitorenter 指令时，将会尝试获取对象所对应的 monitor 的所有权，即尝试获得对象的锁。
--- src/main/java/com/salary/java/juc/_004/lock/synchronized01/Synchronized01.java
+++ src/main/java/com/salary/java/juc/_004/lock/synchronized01/Synchronized01.java
@@ -1,4 +1,45 @@
 package com.salary.java.juc._004.lock.synchronized01;
 
+import java.util.concurrent.TimeUnit;
+
 public class Synchronized01 {
+
+    private static int i = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        final Synchronized01 synchronized01 = new Synchronized01();
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                public void run() {
+                    synchronized01.increase();
+                }
+            }).start();
+        }
+        TimeUnit.SECONDS.sleep(6L);
+        System.out.println("" + i);
+        for (int i = 0; i < 3; i++) {
+            new Thread(new Runnable() {
+                public void run() {
+                    synchronized01.decrease();
+                }
+            }).start();
+        }
+        TimeUnit.SECONDS.sleep(6L);
+        System.out.println("" + i);
+    }
+
+
+    public void increase() {
+        synchronized (this) {
+            ++i;
+        }
+    }
+
+
+    public void decrease() {
+        synchronized (this) {
+            --i;
+        }
+    }
+
 }
