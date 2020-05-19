2b9958527e38f6b0d0acff5a95dce3e53f81fe20
liugang <540184059@qq.com>
2020-05-19 15:35:35 +0800

    线程状态图片

--- src/main/java/com/salary/java/juc/_004/lock/synchronized02/Synchronized02.java
+++ src/main/java/com/salary/java/juc/_004/lock/synchronized02/Synchronized02.java
@@ -1,4 +1,60 @@
-package com.salary.java.juc._004.lock.synchronized02;
+package com.salary.java.juc._004.lock.synchronized01;
+
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.concurrent.TimeUnit;
 
 public class Synchronized02 {
+
+    private static int i = 0;
+
+    public static void main(String[] args) throws Exception {
+
+        final Synchronized02 synchronized02 = new Synchronized02();
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    synchronized02.increase();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    synchronized02.decrease();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        TimeUnit.SECONDS.sleep(6L);
+        System.out.println("" + i);
+    }
+
+
+    public void increase() throws InterruptedException {
+        System.out.println("befor increase thread " + ClassLayout.parseInstance(this).toPrintable());
+        synchronized (this) {
+            final int j = i;
+            System.out.println("increase thread " + j + "" + ClassLayout.parseInstance(this).toPrintable());
+            ++i;
+            TimeUnit.SECONDS.sleep(3L);
+        }
+    }
+
+
+    public void decrease() throws InterruptedException {
+        System.out.println("befor decrease thread " + "" + ClassLayout.parseInstance(this).toPrintable());
+        synchronized (this) {
+            final int j = i;
+            System.out.println("decrease thread " + j + "" + ClassLayout.parseInstance(this).toPrintable());
+            --i;
+            TimeUnit.SECONDS.sleep(3L);
+        }
+    }
+
 }
