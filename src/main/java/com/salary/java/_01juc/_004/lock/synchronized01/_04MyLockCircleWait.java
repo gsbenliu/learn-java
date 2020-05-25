bdf49b6ba86064de1f657d1396b31d5b53d172b1
liugang <540184059@qq.com>
2020-05-25 22:29:20 +0800

    增加wait+自旋的锁实现

--- src/main/java/com/salary/java/_01juc/_004/lock/synchronized01/_04MyLockCircleWait.java
+++ src/main/java/com/salary/java/_01juc/_004/lock/synchronized01/_04MyLockCircleWait.java
@@ -1,6 +1,6 @@
 package com.salary.java._01juc._004.lock.synchronized01;
 
-public class _04MyLockCirclePark {
+public class _04MyLockCircleWait {
 
     public static void main(String[] args) {
         
---
+++ src/main/java/com/salary/java/_01juc/_004/lock/synchronized01/_05MyLockCirclePark.java
@@ -0,0 +1,10 @@
+package com.salary.java._01juc._004.lock.synchronized01;
+
+public class _05MyLockCirclePark {
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
